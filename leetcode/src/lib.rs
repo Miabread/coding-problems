79de964274b306550806c4eb73f9b9f9543e467b
jamesBeeProg <jamesbeeprog@gmail.com>
2021-07-28 14:18:47 -0400

    Leetcode: number_of_1_bits

--- leetcode/src/lib.rs
+++ leetcode/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod longest_common_prefix;
+pub mod number_of_1_bits;
 pub mod pascals_triangle;
 pub mod remove_element;
 pub mod single_number;
---
+++ leetcode/src/number_of_1_bits.rs
@@ -0,0 +1,28 @@
+pub fn shifts(mut n: u32) -> i32 {
+    let mut count = 0;
+    for _ in 0..32 {
+        if n % 2 == 1 {
+            count += 1;
+        }
+        n >>= 1;
+    }
+    count
+}
+
+pub fn string(n: u32) -> i32 {
+    format!("{:b}", n).chars().filter(|it| *it == '1').count() as i32
+}
+
+#[test]
+fn test_shifts() {
+    assert_eq!(3, shifts(0b00000000000000000000000000001011));
+    assert_eq!(1, shifts(0b00000000000000000000000010000000));
+    assert_eq!(31, shifts(0b11111111111111111111111111111101));
+}
+
+#[test]
+fn test_string() {
+    assert_eq!(3, string(0b00000000000000000000000000001011));
+    assert_eq!(1, string(0b00000000000000000000000010000000));
+    assert_eq!(31, string(0b11111111111111111111111111111101));
+}
