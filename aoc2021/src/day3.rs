1ea5e1ed75c0b7af4df2e6e6213e75a18177e992
jamesBeeProg <jamesbeeprog@gmail.com>
2021-12-03 22:33:37 -0500

    Start day3 part2

--- aoc2021/src/day3.rs
+++ aoc2021/src/day3.rs
@@ -24,11 +24,7 @@ pub fn part1(input: &[Vec<bool>]) -> usize {
     let gamma: usize = (0..width)
         .map(|column| {
             // Get the amount of trues in that column
-            let true_count = input
-                .iter()
-                .map(|row| row[column])
-                .filter(|bool| *bool)
-                .count();
+            let true_count = input.iter().filter(|row| row[column]).count();
 
             // Whatever wasn't true was false
             let false_count = height - true_count;
@@ -64,11 +60,57 @@ pub fn part1(input: &[Vec<bool>]) -> usize {
     gamma * epsilon
 }
 
+// INCOMPLETE
+#[aoc(day3, part2)]
+pub fn part2(input: &[Vec<bool>]) -> usize {
+    // Make table dimensions explicit
+    let height = input.len();
+    let width = input[0].len();
+
+    let mut row_flags = vec![true; height];
+    let mut column = 0;
+
+    let oxygen = loop {
+        let true_count = row_flags
+            .iter()
+            .zip(input.iter())
+            .filter(|(flag, row)| **flag && row[column])
+            .count();
+
+        let false_count = height - true_count;
+
+        let greatest_value = true_count >= false_count;
+
+        let rows_left = row_flags
+            .iter_mut()
+            .zip(input.iter())
+            .filter(|(flag, _)| **flag)
+            .filter_map(|(flag, row)| {
+                *flag = row[column] == greatest_value;
+                flag.then(|| ())
+            })
+            .count();
+
+        if rows_left == 1 {
+            let index = row_flags.iter().position(|it| *it).unwrap();
+            break input[index]
+                .iter()
+                .rev()
+                .enumerate()
+                .filter_map(|(place, is_set)| is_set.then(|| 1 << place))
+                .sum();
+        }
+
+        column += 1;
+    };
+    dbg!(oxygen)
+}
+
 #[cfg(test)]
 pub mod test {
-    #[test]
-    pub fn part1() {
-        let input = "00100
+    use super::*;
+
+    const INPUT: &str = "00100
 11110
 10110
 10111
@@ -80,6 +122,14 @@ pub mod test {
 11001
 00010
 01010";
-        assert_eq!(super::part1(&super::parse(input)), 198);
+
+    #[test]
+    pub fn test_part1() {
+        assert_eq!(part1(&parse(INPUT)), 198);
+    }
+
+    #[test]
+    pub fn test_part2() {
+        assert_eq!(part2(&parse(INPUT)), 23);
     }
 }
